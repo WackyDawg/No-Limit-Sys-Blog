09ac5c2d99bd20689a372931cfd8fb14db677001
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-01-08 21:48:56 -0800

    Fixed Installation

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,26 @@
+const connectDB = require('../config/db');
 const Category = require('../models/categoryModel');
 const Post = require('../models/postModel');
-const Setting = require('../models/blogSettingsModel')
+const Setting = require('../models/blogSettingsModel');
+const Contact = require('../models/contactModel')
+const installController = require('../controller/installController');
+const setupCheckMiddleware = require('../middleware/setupCheck');
+const fs = require('fs');
+const setupLayout = '../views/layouts/setup';
 
 exports.getIndex = async (req, res) => {
     try {
-      // Fetch all categories
-      const allCategories = await Category.find();
+      const locals = {
+        title: "",
+        description: ""
+      }
+
+      const db = await connectDB().catch(err => {
+        console.error(err);
+        throw new Error('Failed to connect to the database');
+    });
+          // Fetch all categories
+      const allCategories = await Category.find().maxTimeMS(30000); // 30 seconds timeout
   
       // Fetch categories with post counts
       const categoryCounts = await Promise.all(allCategories.map(async (category) => {
@@ -42,6 +57,16 @@ exports.getIndex = async (req, res) => {
         ...post._doc,
         category: post.category ? post.category.name : 'Uncategorized',
       }));
+      const isInstalled = fs.existsSync('.env') && fs.existsSync('installed.txt');
+
+        if (!isInstalled) {
+          // If the application is not installed, render a specific page
+          return res.render('installation/index', { layout: setupLayout});
+        }
+
+        // Proceed with your regular route handling
+        // For example, you can render your main application page
+
   
       res.render('index', {
         categories: categoryCounts,
@@ -62,4 +87,87 @@ exports.getIndex = async (req, res) => {
     }
   };
   
+  exports.getContact = async (req, res) => {
+    try {
+      const locals = {
+        title: "Contact",
+        description: ""
+      }
+      const allPosts = await Post.find().populate('category', 'name');
+      const existingSetting = await Setting.find({});
+      res.render('Contact', { posts: allPosts, existingSetting, locals})
+    } catch (error) {
+      
+    }
+  }
+
+  exports.postContact = async (req, res) => {
+    try {
+      if (!req.body.name || !req.body.email || !req.body.phone || !req.body.subject || !req.body.message) {
+        return res.status(400).send("Every field is required");
+      }
+  
+      const { name, email, phone, subject, message } = req.body;
+  
+      const newContact = new Contact({
+        name: name,
+        email: email,
+        phone: phone,
+        subject: subject,
+        message: message,
+      });
+  
+      await newContact.save(); // Use save() instead of Save()
+  
+      res.redirect('/contact');
+    } catch (error) {
+      console.error(error);
+      res.status(500).render('error/500'); // Render a '500' error page for server errors
+    }
+  };
+  
+
+  exports.getAllBlogPost = async (req, res, next) => {
+    try {
+      const locals = {
+        title: "Blog",
+        description: ""
+      }
+      const db = await connectDB();
+        // Get the current page from the query parameters, default to 1 if not provided
+        const page = parseInt(req.query.page) || 1;
+        // Set the number of posts per page
+        const postsPerPage = 10;
+
+        // Fetch total number of blog posts (for calculating total pages)
+        const totalPosts = await Post.countDocuments();
+
+        // Calculate the total number of pages
+        const totalPages = Math.ceil(totalPosts / postsPerPage);
+
+        // Calculate the skip value based on the current page
+        const skip = (page - 1) * postsPerPage;
+
+        // Fetch 10 blog posts for the current page
+        const allPosts = await Post.find()
+            .populate('category', 'name')
+            .skip(skip)
+            .limit(postsPerPage);
+
+        const existingSetting = await Setting.find({});
+
+        // Calculate the starting count value for the current page
+        const startingCount = (page - 1) * postsPerPage + 1;
+
+        // Pass the blog posts data and pagination information to the template
+        res.render("allPosts", { posts: allPosts,locals, existingSetting, currentPage: page, totalPages, startingCount });
+    } catch (error) {
+        console.error(error);
+        // Handle errors appropriately
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+
+
 
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const home = express.Router();
 const homeController = require('../controller/homeController');
+const setupCheckMiddleware = require('../middleware/setupCheck');
+
 
 home.get('/', homeController.getIndex);
+home.get('/blog', homeController.getAllBlogPost);
+home.get('/Contact', homeController.getContact);
+home.post('/contact', homeController.postContact);
 // category.get('/:id', categoryController.getCategoryById);
 // category.post('/', categoryController.createCategory);
 // category.put('/:id', categoryController.updateCategory);
