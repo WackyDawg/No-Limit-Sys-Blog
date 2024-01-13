1748e5c8bee0a3db5437c442686b1b18a1282d84
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-01-12 17:55:01 -0800

    feat: Add MongoDB connection setup and version retrieval
    - Establishes a connection to MongoDB using Mongoose
    - Fetches and logs the MongoDB server version
    - Includes connection options for better compatibility

--- src/config/db.js
+++ src/config/db.js
@@ -7,16 +7,26 @@ const connectDB = async () => {
   const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.oyimqiz.mongodb.net/${mongoDatabase}`;
 
   try {
-    await mongoose.connect(mongoURI, {
+    const connectionOptions = {
       useNewUrlParser: false, // useNewUrlParser is deprecated
       useUnifiedTopology: true,
       connectTimeoutMS: 30000,
-    });
+    };
+
+    const connection = await mongoose.connect(mongoURI, connectionOptions);
     console.log('Connected to MongoDB');
 
-    const db = mongoose.connection;
+    // Fetch MongoDB version
+    const client = mongoose.connection.getClient(); // Get the underlying MongoDB driver client
+    const adminDb = client.db().admin();
+    const mongoVersion = await adminDb.serverInfo();
+    const versionString = mongoVersion.version;
+    //console.log(mongoVersion);
 
-    return db;
+    return {
+      db: connection.connection,
+      mongoVersion: versionString,
+    };
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
