d2c4b3e479ae88b3ed4ae3f31984583dde104fc6
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-01-15 17:02:31 -0800

    route change

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -1114,7 +1114,7 @@ exports.sendBulkMail = async (req, res) => {
         res.status(500).send("Error sending email");
       } else {
         console.log("Email sent: ", info.response);
-        res.status(200).send("Email sent successfully");
+        res.status(200).render('/admin/newsletter');
       }
     });
   } catch (error) {
